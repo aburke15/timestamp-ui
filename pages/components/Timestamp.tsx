5b04b88999301526a319d8bf2dce81eb6d6180db
Andre Burke <aburke.dev@gmail.com>
2022-01-08 00:42:43 -0700

    added some styling to timestamp component

--- pages/components/Timestamp.tsx
+++ pages/components/Timestamp.tsx
@@ -1,7 +1,12 @@
 import { NextPage } from "next";
 
 const Timestamp: NextPage = () => {
-  return <div>Hello</div>;
+  return (
+    <div className="container text-center mt-5">
+      <h2>Timestamp Microservice</h2>
+      <div className="g-underline"></div>
+    </div>
+  );
 };
 
 export default Timestamp;
