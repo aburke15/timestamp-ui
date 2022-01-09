6f6c91bb4ba3d0f2dde2051b8dcf8eff54754d14
Andre Burke <aburke.dev@gmail.com>
2022-01-08 19:23:23 -0700

    revised a bunch of stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    import("bootstrap/dist/js/bootstrap.min.js");
+    import("../node_modules/bootstrap/dist/js/bootstrap.min.js");
   }, []);
   return <Component {...pageProps} />;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,37 @@
 import type { NextPage } from "next";
-import Navbar from "./components/Navbar";
-import Timestamp from "./components/Timestamp";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <Navbar />
-      <Timestamp />
+    <div className="container text-center justify-content-center">
+      <div className="row mt-2">
+        <h2>Timestamp Microservice</h2>
+        <div className="g-underline"></div>
+      </div>
+      <div className="row mt-5">
+        <h4>Example Usage:</h4>
+        <ul>
+          <li>
+            <a href="">Hello World</a>
+          </li>
+          <li>
+            <a href="">Hello Test</a>
+          </li>
+        </ul>
+      </div>
+      <div className="row mt-5">
+        <h4>Example Output:</h4>
+      </div>
+      <footer className="mt-5">
+        By{" "}
+        <a
+          href="https://github.com/aburke15"
+          target={"_blank"}
+          rel="noopener noreferrer"
+        >
+          Andre Burke
+        </a>
+      </footer>
     </div>
   );
 };
